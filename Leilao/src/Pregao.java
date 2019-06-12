842b51339c738b4f5a09b4b9238e29585ddad554
B4ng <48741497+theN1ghtFury@users.noreply.github.com>
2019-06-12 13:14:58 -0300

    Add files via upload

--- Leilao/src/Pregao.java
+++ Leilao/src/Pregao.java
@@ -26,6 +26,7 @@ public class Pregao {
 	}
 	
 	public void iniciaPregao() { 			//Inicia ou reinicia o tempo do pregao
+		if(!this.situacao) {this.situacao = true;}
 		this.hora_inicio = Instant.now();
 	}
 	
